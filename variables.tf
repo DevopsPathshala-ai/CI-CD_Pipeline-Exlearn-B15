c35654c27709a524bbb4914f03b1047eae79a7f8
mangeshchauhan <mangesh.225@gmail.com>
2026-03-29 12:55:04 +0530

    updating the variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
@@ -20,4 +19,8 @@ variable "private_subnet_cidr" {
   description = "CIDR for Private Subnet"
   type        = string
   default     = "10.1.2.0/24"
+}
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
 }
